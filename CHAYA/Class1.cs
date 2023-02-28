e4e09246c792a4e282aa3e87138dba39764b720b
chayareem <113825594+chayareem@users.noreply.github.com>
2023-02-28 14:50:49 +0200

    Update Class1.cs

--- CHAYA/Class1.cs
+++ CHAYA/Class1.cs
@@ -1,7 +1,22 @@
 ﻿namespace CHAYA
 {
-    public class Class1
+        public class ExternalApiService
     {
+        public async Task<string> Connect()
+        {
+            using (var client = new HttpClient())
+            {
+                var obj = new { fullName = "sdafsdf dFSA", yearOfBirth = 2000, tz = "123456789" };
+                var arr = new List<object>();
+                arr.Add(obj);
+                var jsonObj = new StringContent(JsonConvert.SerializeObject(arr), Encoding.UTF8, "application/json");
+                var result = await client.PostAsync("https://api-sq.azurewebsites.net/people", jsonObj);
 
+                var str = await result.Content.ReadAsStringAsync();
+
+                return str;
+            }
+        }
+    }
     }
 }
